6d52dd99e3632ebd5e8bf1f1def449b0aae4c7fb
Peter Johnson <peter@tortall.net>
2001-01-09 22:39:15 +0000

    Added parsing of EX291 environment variable. Removed overriding int10h, ax=4F00h function. Many other minor changes.

--- src/dos/extra291.asm
+++ src/dos/extra291.asm
@@ -1,5 +1,5 @@
 ; extra291.asm -- Extra BIOS services for ECE 291 loader
-; Copyright (C) 2000 Peter Johnson
+; Copyright (C) 2000-2001 Peter Johnson
 ;
 ; Use NASM to assemble.
 ;
@@ -23,20 +23,18 @@
 ; Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 ;
 ; This is the 16 bit DOS TSR portion of the Extra BIOS services for ECE 291
-; for Windows NT/2000. It will register the NT dll, hook various interrupts,
-; and then terminate and stay resident Then it waits for interrupt calls.
+; for Windows 2000. It will register the DLL, hook various interrupts,
+; and then terminate and stay resident. Then it waits for interrupt calls.
 ; Any calls will be first handled here, and then redirected to the DLL. Some
 ; calls need special processing in both the DLL and here.
 ;
-;  ";;;;" meens code that is not neccessary
-;
-; $Id: extra291.asm,v 1.2 2000/12/18 06:28:00 pete Exp $
+; $Id: extra291.asm,v 1.3 2001/01/09 22:39:15 pete Exp $
 %include "nasm_bop.inc"
 
 %macro printstr 1
 	mov	ah, 9h
 	mov	dx, %1
-	int	21h               
+	int	21h
 %endmacro
 
 ;; dispatch what, where
@@ -51,9 +49,9 @@
 %endmacro
 
 %macro call_vdd 0
-        shl 	eax, 16             
-        mov 	ax, [cs:dllHandle]  
-        VDD_Dispatch             
+	shl 	eax, 16
+	mov 	ax, [cs:dllHandle]
+	VDD_Dispatch
 %endmacro
 
 %macro mutex_lock 1
@@ -105,7 +103,7 @@ LOADER_PRODUCTBUILD  		EQU	58
 
 MAX_SIZE			equ	260
 
-DRIVER_VERSION			equ	0001h
+DRIVER_VERSION			equ	0100h
 	
 	ORG	100h
 	jmp	Do_TSR_Load
@@ -113,37 +111,24 @@ DRIVER_VERSION			equ	0001h
 dllHandle	dw	0	; return value of Register Module, used by Dispatch 
 oldint10h	dd	0
 oldint33h	dd	0
-oldint74h	dd	0
+oldmouseirq	dd	0
 
-inDispatch	db	0
+inDispatch	db	0	; These variables mapped by the VDD.  Don't move/reorder!
 VDD_int_wait	db	0
 DOS_VDD_Mutex	db	0
-%ifdef WINDOWEDMODE
-WindowedMode	db	1
-%else
 WindowedMode	db	0
-%endif
+Mouse_IRQ	db	0
+Keyboard_IRQ	db	0
+Keyboard_Port	dw	0
+
+Mouse_INT	db	0
+Keyboard_INT	db	0
 
 inGraphics	db	0
 
 userMouseCallback	dd	0
 
 old_video_mode	db	0
-
-	STRUC	VESAInfo	; VESA information block
-.Signature		resb	4
-.Version		resw	1
-.OEMStringPtr		resd	1
-.Capabilities		resb	4
-.VideoModePtr		resd	1
-.TotalMemory		resw	1
-.OEMSoftwareRev		resw	1
-.OEMVendorNamePtr	resd	1
-.OEMProductNamePtr	resd	1
-.OEMProductRevPtr	resd	1
-.Reserved		resb	222
-.OEMData		resb	256
-	ENDSTRUC
 
 	STRUC	VESASupp	; VESA supplemental info block
 .Signature		resb	7
@@ -158,12 +143,10 @@ old_video_mode	db	0
 	ENDSTRUC
 
 ; VESA information
-vesa_oem_name	db	'ECE 291',0
-vesa_mode_list	dw	0FFFFh
-; VBE 2.0 information
+vesa_oem_name		db	'ECE 291',0
 vesa_vendor_name	db	'ECE 291',0
 vesa_product_name	db	'Extra BIOS Services',0
-vesa_rev_string		db	'Revision 0.1',0
+vesa_rev_string		db	'Revision 1.0',0
 
 ;----------------------------------------
 ; Interrupt10_Handler
@@ -179,67 +162,14 @@ Interrupt10_Handler
 	cmp	al, [cs:old_video_mode]
 	je	near .UnSetVBEMode_with_oldBIOS
 .dodispatch:
-	dispatch	4F00h, .GetSVGAinfo
-	dispatch	4F23h, near .Supplemental291API
+	dispatch	4F23h, .Supplemental291API
 .original:
 	popf
 	jmp	word far [cs:oldint10h]		; jump to BIOS interrupt handler
 
-.GetSVGAinfo:
-	; ES:DI => pointer to buffer
-	mov	word [es:di+VESAInfo.Version], 0300h			; Version Number
-	mov	word [es:di+VESAInfo.OEMStringPtr], vesa_oem_name	; OEM Name offset
-	mov	word [es:di+VESAInfo.OEMStringPtr+2], cs		; OEM Name segment
-	xor	ax, ax
-	mov	word [es:di+VESAInfo.Capabilities], ax			; Capabilities flags
-	mov	word [es:di+VESAInfo.Capabilities+2], ax
-	mov	word [es:di+VESAInfo.VideoModePtr], vesa_mode_list	; Mode list offset
-	mov	word [es:di+VESAInfo.VideoModePtr+2], cs		; Mode list segment
-	mov	word [es:di+VESAInfo.TotalMemory], 0FFFFh		; # 64k pages of video RAM
-	mov	word [es:di+VESAInfo.OEMSoftwareRev], DRIVER_VERSION	; OEM software version
-	mov	word [es:di+VESAInfo.OEMVendorNamePtr], vesa_vendor_name
-	mov	word [es:di+VESAInfo.OEMVendorNamePtr+2], cs
-	mov	word [es:di+VESAInfo.OEMProductNamePtr], vesa_product_name
-	mov	word [es:di+VESAInfo.OEMProductNamePtr+2], cs
-	mov	word [es:di+VESAInfo.OEMProductRevPtr], vesa_rev_string
-	mov	word [es:di+VESAInfo.OEMProductRevPtr+2], cs
-
-	; Pass uninstall information if incoming Signature='VBE2'
-	cmp	dword [es:di+VESAInfo.Signature], 'VBE2'
-	jne	.GetSVGAinfo_done
-	mov	word [es:di+VESAInfo.OEMData], 1
-	mov	ax, [cs:oldint10h]
-	mov	word [es:di+VESAInfo.OEMData+2], ax
-	mov	ax, [cs:oldint10h+2]
-	mov	word [es:di+VESAInfo.OEMData+4], ax
-	mov	ax, [cs:oldint33h]
-	mov	word [es:di+VESAInfo.OEMData+6], ax
-	mov	ax, [cs:oldint33h+2]
-	mov	word [es:di+VESAInfo.OEMData+8], ax
-	mov	ax, [cs:oldint74h]
-	mov	word [es:di+VESAInfo.OEMData+10], ax
-	mov	ax, [cs:oldint74h+2]
-	mov	word [es:di+VESAInfo.OEMData+12], ax
-	mov	ax, [cs:dllHandle]
-	mov	word [es:di+VESAInfo.OEMData+14], ax
-
-.GetSVGAinfo_done:
-	; Call VDD to determine free memory
-	push	eax
-	mov	ax, GET_MEMORY
-	call_vdd
-	mov	word [es:di+VESAInfo.TotalMemory], ax
-	pop	eax
-	
-	; Indicate success to caller
-	mov	dword [es:di+VESAInfo.Signature], 'VESA'
-	mov	ax, 004Fh
-	popf
-	retf	2
-
 .Supplemental291API:
 	dispatchAPI	00h, .GetSupplementalInfo
-	dispatchAPI	02h, .SetVBEMode
+	dispatchAPI	02h, near .SetVBEMode
 	dispatchAPI	03h, near .UnSetVBEMode
 	dispatchAPI	04h, near .RefreshScreen
 	jmp	.original
@@ -264,6 +194,25 @@ Interrupt10_Handler
 	mov	word [es:di+VESASupp.OEMProductRevPtr+2], cs
 	mov	word [es:di+VESASupp.OEMStringPtr], vesa_oem_name	; OEM Name offset
 	mov	word [es:di+VESASupp.OEMStringPtr+2], cs		; OEM Name segment
+
+	; Pass uninstall information in reserved area
+	mov	word [es:di+VESASupp.Reserved], 1
+	mov	ax, [cs:oldint10h]
+	mov	word [es:di+VESASupp.Reserved+2], ax
+	mov	ax, [cs:oldint10h+2]
+	mov	word [es:di+VESASupp.Reserved+4], ax
+	mov	ax, [cs:oldint33h]
+	mov	word [es:di+VESASupp.Reserved+6], ax
+	mov	ax, [cs:oldint33h+2]
+	mov	word [es:di+VESASupp.Reserved+8], ax
+	mov	ax, [cs:oldmouseirq]
+	mov	word [es:di+VESASupp.Reserved+10], ax
+	mov	ax, [cs:oldmouseirq+2]
+	mov	word [es:di+VESASupp.Reserved+12], ax
+	mov	ax, [cs:Mouse_INT]
+	mov	word [es:di+VESASupp.Reserved+14], ax
+	mov	ax, [cs:dllHandle]
+	mov	word [es:di+VESASupp.Reserved+16], ax
 
 	; Indicate success to caller
 	mov	ax, 004Fh
@@ -403,17 +352,17 @@ Interrupt33_Handler
 	jmp	short .callvdd
 
 ;----------------------------------------
-; Interrupt74_Handler
-; Purpose: Processes IRQ 12 signals generated by Win32 driver.
+; MouseIRQ_Handler
+; Purpose: Processes Mouse IRQ signals generated by VDD.
 ;          Jumps to old handler if not in graphics mode.
 ;----------------------------------------
-Interrupt74_Handler
+MouseIRQ_Handler
 	pushf
 	cmp	byte [cs:inGraphics], 0
 	jnz	.ourhandler
 
 	popf
-	jmp	word far [cs:oldint74h]		; jump to old handler
+	jmp	word far [cs:oldmouseirq]	; jump to old handler
 
 .ourhandler:
 	
@@ -435,7 +384,10 @@ Interrupt74_Handler
 .nocallback:
 
 	mov	al, 20h
+	cmp	byte [cs:Mouse_IRQ], 8
+	jb	.lowirq
 	out	0A0h, al
+.lowirq:
 	out	20h, al
 
 	pop	di
@@ -450,19 +402,20 @@ Interrupt74_Handler
 resident_end
 ;; Start of non-resident section
 
-versionmsg1	db	'Extra BIOS services for ECE 291 v0.1, for Windows NT/2000',13,10,'$'
-versionmsg2	db	'Copyright (C) 2000 Peter Johnson',13,10,'$'
+versionmsg1	db	'Extra BIOS services for ECE 291 v1.0, for Windows 2000',13,10,'$'
+versionmsg2	db	'Copyright (C) 2000-2001 Peter Johnson',13,10,'$'
 versionmsg3	db	'BETA SOFTWARE, ABSOLUTELY NO IMPLIED OR EXPRESSED WARRANTY',13,10,'$'
 
 ;startmsg	db	'Searching module ... ','$'
-MustUnderNT	db	'This program requires Microsoft Windows NT/2000, terminating ...',13,10,'$'
+MustUnderNT	db	'This program requires Microsoft Windows 2000, terminating ...',13,10,'$'
 
 ;; error codes from Register Module
-error1		db	'DLL not found, terminating ...',13,10,'$'
-error2		db	'Dispatch routine not found, terminating ...',13,10,'$'
-error3		db	'Initialization routine not found, terminating ...',13,10,'$'
-error4		db	'Out of memory, terminating ...',13,10,'$'
-error5		db	'Sanity check: didnt get uninstall info even though driver is installed ...',13,10,'$'
+errorNoDLL	db	'DLL not found, terminating ...',13,10,'$'
+errorNoDispatch	db	'Dispatch routine not found, terminating ...',13,10,'$'
+errorNoInit	db	'Initialization routine not found, terminating ...',13,10,'$'
+errorNoMem	db	'Out of memory, terminating ...',13,10,'$'
+errorNoUninst	db	'Sanity check: didnt get uninstall info even though driver is installed ...',13,10,'$'
+errorBadEnv	db	'Missing or invalid EX291 environment variable, terminating ...',13,10,'$'
 
 wrongversion	db	'DLL & COM files version mismatch, terminating ...',13,10,'$'
 errorUnknown	db	'Unknown error during module registration, terminating ...,',13,10,'$'
@@ -475,8 +428,202 @@ dllDispatch	db	'Extra291Dispatch',0
 		db	13,10,'$'
 dllInitialize	db	'Extra291RegisterInit',0
 		db	13,10,'$'
+envString	db	0,'EX291'
 
-Do_TSR_Load	; CS=DS=ES
+;----------------------------------------
+; Load_EX291_Settings
+; Purpose: Parses EX291 environment variable.
+;----------------------------------------
+Load_EX291_Settings
+	push	cs
+	pop	ds
+
+	mov	di, 80h
+	mov	ah, 51h
+	int	21h			; get Program Segment Prefix
+
+	mov	es, bx
+	mov	ax, [es:002Ch]		; PSP:002Ch = environment's segment
+	mov	es, ax
+
+	xor	si, si
+	xor	di, di
+
+.GetEnv:
+	mov	al, [es:di]		; search for 0, which comes before
+	cmp	al, [envString+si]	;   each environment entry.
+	je	.FoundOne
+	xor	si, si
+	inc	di
+	cmp	di, 0FFFFh
+	jne	.GetEnv
+
+.FoundOne:
+	inc	si			; upon finding the 0, check if the
+	inc	di			;   rest of EX291 matches.
+	cmp	si, 6
+	jne	.GetEnv
+
+	inc	di
+	mov	cx, 0
+
+.FindSettingsLoop:			; Now parse the line like w K7,300 M9 for current settings.
+	xor	al, al			; clear windowed flag
+	cmp	byte [es:di], 's'
+	je	Settings_ScreenMode
+	cmp	byte [es:di], 'S'
+	je	Settings_ScreenMode
+	inc	al			; set windowed flag
+	cmp	byte [es:di], 'w'
+	je	Settings_ScreenMode
+	cmp	byte [es:di], 'W'
+	je	Settings_ScreenMode
+
+	cmp	byte [es:di], 'K'
+	je	Settings_Keyboard
+	cmp	byte [es:di], 'k'
+	je	Settings_Keyboard
+
+	cmp	byte [es:di], 'M'
+	je	near Settings_MouseIRQ
+	cmp	byte [es:di], 'm'
+	je	near Settings_MouseIRQ
+
+	cmp	cx, 0111b
+	je	.AllFound
+	cmp	byte [es:di], 0
+	je	.EnvError
+	inc	di
+	jmp	.FindSettingsLoop
+
+.AllFound:
+	xor	ax, ax
+	ret
+
+.EnvError:
+	xor	ax, ax
+	inc	ax
+	ret
+
+Settings_ScreenMode:
+	mov	[WindowedMode], al
+	or	cx, 0100b
+	inc	di
+	jmp	Load_EX291_Settings.FindSettingsLoop
+
+Settings_Keyboard:
+	; First read Keyboard IRQ
+	mov	bx, 1			; bx points to 1's place
+	xor	ah, ah			; zero 10's place
+	mov	al, [es:di+2]		; is there a second digit?
+	cmp	al, '0'
+	jb	.readones
+	cmp	al, '9'
+	ja	.readones
+	inc	bx			; point to next digit
+	mov	al, [es:di+1]		; read 10's digit
+	sub	al, '0'
+	cmp	al, 9			; check range
+	ja	near .EnvError
+	mov	ah, al			; ah=al*10
+	shl	ah, 2
+	add	ah, al
+	shl	ah, 1
+.readones:
+	mov	al, [es:di+bx]		; read 1's digit
+	sub	al, '0'
+	cmp	al, 9			; check range
+	ja	near .EnvError
+	add	al, ah			; add in 10's (if any) to get IRQ
+	mov	[Keyboard_IRQ], al
+	cmp	al, 8			; high IRQ?
+	jb	.lowirq
+	add	al, 70h-8h-8h		; high IRQ maps to 70h.. subtract extra 8 for following add
+.lowirq:
+	add	al, 8h			; low IRQ maps to 8h
+	mov	[Keyboard_INT], al
+	or	cx, 100b		; got mouse component
+	add	di, bx			; move to next component
+	inc	di
+
+	cmp	byte [es:di], ','	; Check for , delimiter
+	jne	.EnvError
+
+	; Read Keyboard I/O Port
+	mov	ax, 0
+	mov	bl, [es:di+3]		; get lowest nibble
+	sub	bl, '0'
+	cmp	bl, 9			; check range
+	ja	.EnvError
+	or	al, bl
+	mov	bl, [es:di+2]		; get middle nibble
+	sub	bl, '0'
+	cmp	bl, 9			; check range
+	ja	.EnvError
+	shl	bl, 4
+	or	al, bl
+	mov	bl, [es:di+1]		; get high nibble
+	sub	bl, '0'
+	cmp	bl, 9			; check range
+	ja	.EnvError
+	or	ah, bl
+	mov	[Keyboard_Port], ax
+	or	cx, 010b
+	add	di, 4			; move to next component
+	jmp	Load_EX291_Settings.FindSettingsLoop
+.EnvError:
+	xor	ax, ax
+	inc	ax
+	ret
+
+Settings_MouseIRQ:
+	mov	bx, 1			; bx points to 1's place
+	xor	ah, ah			; zero 10's place
+	mov	al, [es:di+2]		; is there a second digit?
+	cmp	al, '0'
+	jb	.readones
+	cmp	al, '9'
+	ja	.readones
+	inc	bx			; point to next digit
+	mov	al, [es:di+1]		; read 10's digit
+	sub	al, '0'
+	cmp	al, 9			; check range
+	ja	.EnvError
+	mov	ah, al			; ah=al*10
+	shl	ah, 2
+	add	ah, al
+	shl	ah, 1
+.readones:
+	mov	al, [es:di+bx]		; read 1's digit
+	sub	al, '0'
+	cmp	al, 9			; check range
+	ja	.EnvError
+	add	al, ah			; add in 10's (if any) to get IRQ
+	mov	[Mouse_IRQ], al
+	cmp	al, 8			; high IRQ?
+	jb	.lowirq
+	add	al, 70h-8h-8h		; high IRQ maps to 70h.. subtract extra 8 for following add
+.lowirq:
+	add	al, 8h			; low IRQ maps to 8h
+	mov	[Mouse_INT], al
+	or	cx, 001b		; got mouse component
+	add	di, bx			; move to next component
+	inc	di
+	jmp	Load_EX291_Settings.FindSettingsLoop
+.EnvError:
+	xor	ax, ax
+	inc	ax
+	ret
+
+
+;----------------------------------------
+; Do_TSR_Load
+; Purpose: Loads/unloads resident part of driver.
+;----------------------------------------
+Do_TSR_Load
+	push	cs
+	pop	ds
+
 	printstr   versionmsg1
 	printstr   versionmsg2
 	printstr   versionmsg3
@@ -491,46 +638,47 @@ Do_TSR_Load	; CS=DS=ES
         jmp 	.Terminate	
 
 .check_presence:
-;	printstr   startmsg
-;	printstr   dllName
-	
-	;check if srv already running
-	mov	ax, 4F00h
+	; check if srv already running
+	mov	ax, 4F23h
+	xor	bl, bl
 	push	cs
 	pop	es
 	mov	di, vesa_info_area
-	mov	dword [es:di+VESAInfo.Signature], 'VBE2'
-	mov	word [es:di+VESAInfo.OEMData], 0
+	mov	word [es:di+VESASupp.Reserved], 0
 	int	10h
-	cmp	dword [es:di+VESAInfo.Signature], 'VESA'	; Is any VESA present?
-	jne	.install_dll
-	les	di, [vesa_info_area+VESAInfo.OEMStringPtr]	; Is our driver present?
-	cmp	dword [es:di+VESAInfo.Signature], 'ECE '
-	;services not running
-	jne	.install_dll
-	cmp	word [vesa_info_area+VESAInfo.OEMData], 1		; Did we get the uninstall info?
-	jne	near .Lerror5
+	cmp	ax, 004Fh		; Is there an API for that index?
+	jne	.install
+	cmp	word [es:di+VESASupp.Signature+4], '29'	; Is our driver present?
+	jne	.install
+	cmp	byte [es:di+VESASupp.Signature+6], '1'
+	jne	.install
+	cmp	word [vesa_info_area+VESASupp.Reserved], 1	; Did we get the uninstall info?
+	je	.restore_int
 
-;; restore interrupts
+        printstr errorNoUninst
+	jmp	.Terminate
+
+.restore_int:
+	; restore interrupts
 	mov 	ah, SET_INTERRUPT_VECTOR
 	mov 	al, 10h            	; video Handler
-	lds	dx, [vesa_info_area+VESAInfo.OEMData+2]
+	lds	dx, [vesa_info_area+VESASupp.Reserved+2]
 	int 	21h
 
 	mov 	ah, SET_INTERRUPT_VECTOR
 	mov 	al, 33h            	; mouse Handler
-	lds	dx, [vesa_info_area+VESAInfo.OEMData+6]
+	lds	dx, [vesa_info_area+VESASupp.Reserved+6]
 	int 	21h
 
 	mov 	ah, SET_INTERRUPT_VECTOR
-	mov 	al, 74h            	; IRQ12 Handler
-	lds	dx, [vesa_info_area+VESAInfo.OEMData+10]
+	mov 	al, [vesa_info_area+VESASupp.Reserved+14]	; mouse IRQ Handler
+	lds	dx, [vesa_info_area+VESASupp.Reserved+10]
 	int 	21h
 
-;;;; release VDD
+	; release VDD
 	push	cs
 	pop	ds
-	mov	ax, [vesa_info_area+VESAInfo.OEMData+14]
+	mov	ax, [vesa_info_area+VESASupp.Reserved+16]
 	VDD_UnRegister
 
 	push	cs
@@ -538,11 +686,20 @@ Do_TSR_Load	; CS=DS=ES
 	printstr unregister
 	jmp 	short .Terminate
 
-.install_dll:
+.install:
+	; parse EX291 environment variable
+	call	Load_EX291_Settings
+	test	ax, ax
+	jz	.register_dll
+
+	printstr errorBadEnv
+	jmp	short .Terminate
+
+.register_dll:
 	push	cs
 	pop	es
 	stc
-;;	register Our DLL - From VDD guide for Application Based Intercepts
+	; register Our DLL - From VDD guide for Application Based Intercepts
 	mov 	ax, 0
 	mov 	si, dllName          	;dll name
 	mov 	di, dllInitialize	;init routine
@@ -552,36 +709,27 @@ Do_TSR_Load	; CS=DS=ES
 
 .registerError:
         cmp 	ax, 1
-        je 	.Lerror1
+        jne 	.noterror1
+       	printstr errorNoDLL
+       	jmp 	short .Terminate
+.noterror1:
         cmp 	ax, 2
-        je 	.Lerror2
-        cmp 	ax, 3
-        je 	.Lerror3
-        cmp 	ax, 4
-        je 	.Lerror4
-	printstr errorUnknown
-        jmp 	short .Terminate
-
-.Lerror1:
-        printstr error1
-        jmp 	short .Terminate
-
-.Lerror2:
+	jne	.noterror2
         printstr dllDispatch
-        printstr error2
+        printstr errorNoDispatch
         jmp 	short .Terminate
-
-.Lerror3:
+.noterror2:
+        cmp 	ax, 3
+        jne 	.noterror3
         printstr dllInitialize
-        printstr error3
+        printstr errorNoInit
         jmp 	short .Terminate
-
-.Lerror4:
-        printstr error4
-        jmp 	short .Terminate
-
-.Lerror5:
-        printstr error5
+.noterror3:
+        cmp 	ax, 4
+        jne 	.noterror4
+        printstr errorNoMem
+.noterror4:
+	printstr errorUnknown
 
 .Terminate:
         mov 	ah, TERMINATE_PROGRAM
@@ -632,17 +780,17 @@ Do_TSR_Load	; CS=DS=ES
 	mov 	dx, Interrupt33_Handler
 	int 	21h
 
-	; Install 74h handler
+	; Install mouse IRQ handler
 	mov 	ah, GET_INTERRUPT_VECTOR
-	mov 	al, 74h
+	mov 	al, [Mouse_INT]
 	int 	21h
-	mov 	[oldint74h], bx
-	mov 	[oldint74h+2], es
+	mov 	[oldmouseirq], bx
+	mov 	[oldmouseirq+2], es
 	mov 	ah, SET_INTERRUPT_VECTOR
-	mov 	al, 74h
+	mov 	al, [Mouse_INT]
 	push	cs
 	pop	ds
-	mov 	dx, Interrupt74_Handler
+	mov 	dx, MouseIRQ_Handler
 	int 	21h
 
 	push	cs
@@ -661,4 +809,5 @@ Do_TSR_Load	; CS=DS=ES
 	int 	21h
 
 	SECTION .bss
-vesa_info_area	resb	512
+vesa_info_area	resb	256
+
